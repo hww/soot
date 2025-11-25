39d269b19553456bb9172c0ed95fb39c8a468216
valery <valery.hww@gmail.com>
2025-11-25 14:45:42 +0300

    removed old code

--- common/old/vm/platform.cpp
+++
@@ -1,7 +0,0 @@
-#include "platform.hpp"
-
-namespace vm
-{
-	constexpr Vector Vector::ZERO{ 0,0,0,0 };
-
-}
--- common/old/vm/vm_include.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "vm.hpp"
-#include "variant.hpp"
-#include "string_id.hpp"
-#include "module.hpp"
-#include "native_func.hpp"
-#include "assembler.hpp"
